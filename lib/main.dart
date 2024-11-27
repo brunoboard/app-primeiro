230a4dcb0a8dbba21f0c04f19e5cc4efac3f72c1
boardbruninho <boardbruninho@gmail.com>
2024-11-27 18:34:39 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuAplicativo());
+}
+
+class MeuAplicativo extends StatelessWidget {
+  const MeuAplicativo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const PrimeiraPagina(titulo: 'Primeiro Programa'),
+    );
+  }
+}
+
+class PrimeiraPagina extends StatefulWidget {
+  const PrimeiraPagina({super.key, required this.titulo});
+
+  final String titulo;
+
+  @override
+  State<PrimeiraPagina> createState() => _PrimeiraPaginaState();
+}
+
+class _PrimeiraPaginaState extends State<PrimeiraPagina> {
+  int _contador = 0;
+
+  void _incrementarContador() {
+    setState(() {
+      _contador++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.titulo),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Você pressionou o botão este número de vezes:'),
+            Text(
+              '$_contador',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementarContador,
+        tooltip: 'Incrementar',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
